875eacdd5bf3b079e347a0ed4ff854eb4179da37
ziaochina <ziaochina@gmail.com>
2017-07-11 12:38:24 +0800

    zlj

--- example/index.js
+++ example/index.js
@@ -3,7 +3,7 @@ import myConfig  from './config'
 
 import about from './src/apps/about/index.js'
 import helloWorld from './src/apps/helloWorld/index.js'
-import xr_template_root from './src/apps/xr_template_root/index.js'
+import xr_template_root from './src/apps/xr-template-root/index.js'
 
 const apps = {
 	[about.name]:about,	
